aba09bd9b626939e5bd62bff4080acffdfc27faf
corydave <corydave@users.noreply.github.com>
2021-08-24 09:45:06 -0400

    Create HelloWorld.java

---
+++ CH01/HelloWorld.java
@@ -0,0 +1,9 @@
+// Dave Ghidiu
+// August 24, 2021
+// A simple program to output "Hello world"
+
+public class HelloWorld {
+  public static void main (String[] args) {
+    System.out.println("Hello, World!");
+  }
+}
