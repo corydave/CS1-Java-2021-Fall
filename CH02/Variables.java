6f6b7cc41aff0a135e9b0deae9d6f4e6ccb60339
corydave <corydave@users.noreply.github.com>
2021-09-08 12:19:17 -0400

    Update Variables.java

--- CH02/Variables.java
+++ CH02/Variables.java
@@ -1,1 +1,76 @@
+public class Variables {
 
+     public static void main(String []args){
+        
+        // integers go from -2.5 billion to +2.5 billion
+        int age = 43;
+        int bananasInABunch = 5;
+        
+        // other integer types are:
+        //  - long is 9 quintillion
+        //  - short is +/- 32,000
+        //  - byte is +/- 128
+        
+        
+        
+        
+        
+        // decimals are roughly 64 bits
+        double charliesAge = 3.25;
+        double classAverage = 92.652;
+        
+        // other decimals include:
+        //  - float which is only 32 bits
+        
+        
+        
+        
+        
+        // characters are any keypress
+        char yesOrNo = 'Y';
+        char poop = '💩';
+        
+        
+        
+        
+        
+        // boolean is true or false
+        boolean isThisClassFun = true;
+        boolean isMangoPepsiGood = false;
+        
+        
+        
+        
+        int x = 43;
+        int y;
+        int z;
+        int aa;
+        int bb;
+        
+        
+        int danWang01;
+        int danWang02;
+        int danWang03;
+        double danWang04;
+        char danWang05;
+        
+        
+        int dw1; // your age
+        int dw2; // your height in inches
+        int dw3; // your favorite number
+        double dw4;
+        char dw5;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        System.out.println("GAME OVER MAN!!!!");
+        
+     }
+}
